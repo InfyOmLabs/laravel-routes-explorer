04dc97c1aebf05ba3f8b7a38858ee64c05a2c9c8
Luciano Jr <luciano@lucianojr.com.br>
2017-09-24 15:12:13 +0100

    Fix the relative path to vendor resources

--- src/RoutesExplorerServiceProvider.php
+++ src/RoutesExplorerServiceProvider.php
@@ -13,26 +13,26 @@ class RoutesExplorerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $configPath = __DIR__ . './../config/routes_explorer.php';
+        $configPath = __DIR__ . '/../config/routes_explorer.php';
 
         $this->publishes([
             $configPath => config_path('infyom/routes_explorer.php'),
         ]);
 
-        $migrationPath = __DIR__ . './../stubs/migration.stub';
+        $migrationPath = __DIR__ . '/../stubs/migration.stub';
         $fileName = date('Y_m_d_His') . '_' . 'create_api_calls_count_table.php';
 
         $this->publishes([
             $migrationPath => database_path('migrations/' . $fileName),
         ], 'migrations');
 
-        $viewPath = __DIR__ . './../views/routes.blade.php';
+        $viewPath = __DIR__ . '/../views/routes.blade.php';
 
         $this->publishes([
             $viewPath => resource_path('views/routes/routes.blade.php'),
         ], 'views');
 
-        $this->loadViewsFrom(__DIR__ . './../views', 'infyomlabs');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'infyomlabs');
 
         if (config('infyom.routes_explorer.enable_explorer')) {
             $this->app['router']->get(
